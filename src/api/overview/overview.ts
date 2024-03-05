189745d66e495647bfdd0e8ea730b7b53536fde7
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-03-05 21:43:07 +0800

    feat: Data Statistics

--- src/api/overview/overview.ts
+++ src/api/overview/overview.ts
@@ -1,5 +1,12 @@
 import { fetchDelete, fetchGet, fetchPut } from '@/utils/request'
 
+export interface SumData {
+  topicCount: number
+  replyCount: number
+  commentCount: number
+  userCount: number
+}
+
 export interface OverviewData {
   newTopics: number
   newReplies: number
@@ -10,11 +17,18 @@ export interface OverviewData {
 export interface WeekData {}
 
 type OverviewResponseData = KUNGalgameResponseData<OverviewData>
+type SumResponseData = KUNGalgameResponseData<SumData>
+
+export const getSumDataApi = async (): Promise<SumResponseData> => {
+  const url = `/overview/sum`
+  const response = await fetchGet<SumResponseData>(url)
+  return response
+}
 
 export const getOverviewDataApi = async (
   days: number
 ): Promise<OverviewResponseData> => {
   const url = `/overview?days=${days}`
-  const response = await fetchPut<OverviewResponseData>(url)
+  const response = await fetchGet<OverviewResponseData>(url)
   return response
 }
--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -1,46 +1,81 @@
-import { FC, useState } from 'react'
-import { Statistic, InputNumber, Slider, Row, Col } from 'antd'
-import { getOverviewDataApi } from '@/api/overview/overview'
-import type { OverviewData } from '@/api/overview/overview'
+import { FC, useEffect, useRef, useState } from 'react'
+import { Statistic, Slider, Row, Col, Flex, Divider } from 'antd'
+import { getSumDataApi, getOverviewDataApi } from '@/api/overview/overview'
+import type { SumData, OverviewData } from '@/api/overview/overview'
 
 const OverviewPage: FC = () => {
+  const sum = useRef<SumData>({
+    topicCount: 0,
+    replyCount: 0,
+    commentCount: 0,
+    userCount: 0,
+  })
   const [overview, setOverview] = useState<OverviewData>({
     newTopics: 0,
     newReplies: 0,
     newComments: 0,
     newUsers: 0,
   })
-  const [days, setDays] = useState(1)
+  const [days, setDays] = useState(2)
 
   const onChange = (newValue: number) => {
     setDays(newValue)
   }
 
-  const getOverview = async () => {
-    const res = await getOverviewDataApi(days)
-    setOverview(res.data)
-  }
+  useEffect(() => {
+    const getSum = async () => {
+      const res = await getSumDataApi()
+      sum.current = res.data
+    }
+    getSum()
+  }, [])
+
+  useEffect(() => {
+    const getOverview = async () => {
+      const res = await getOverviewDataApi(days)
+      setOverview(res.data)
+    }
+    getOverview()
+  }, [days])
 
   return (
     <div>
-      <Row>
-        <Col span={12}>
-          <Slider min={1} max={20} onChange={onChange} value={days} />
-        </Col>
+      <h2>数据统计</h2>
+
+      <Flex justify="space-between" className="flex-wrap mt-8">
+        <Statistic title="话题总数" value={sum.current.topicCount} />
+        <Statistic title="回复总数" value={sum.current.replyCount} />
+        <Statistic title="评论总数" value={sum.current.commentCount} />
+        <Statistic title="用户总数" value={sum.current.userCount} />
+      </Flex>
+
+      <Divider />
+
+      <Row justify="space-evenly" align="middle">
         <Col span={4}>
-          <InputNumber
+          <h3>{`${days} 天内数据统计`}</h3>
+        </Col>
+        <Col span={12}>
+          <Slider
+            tooltip={{ open: true, placement: 'left', color: 'blue' }}
             min={1}
-            max={20}
+            max={30}
+            onChange={onChange}
             value={days}
-            onChange={(value) => onChange(value ?? 0)}
           />
         </Col>
       </Row>
 
-      <Statistic title="新发布话题" value={overview.newTopics} />
-      <Statistic title="新发布回复" value={overview.newReplies} />
-      <Statistic title="新发布评论" value={overview.newComments} />
-      <Statistic title="新注册用户" value={overview.newUsers} />
+      <Flex justify="space-between" className="flex-wrap mt-8">
+        <Statistic title="新发布话题" value={overview.newTopics} />
+        <Statistic title="新发布回复" value={overview.newReplies} />
+        <Statistic title="新发布评论" value={overview.newComments} />
+        <Statistic title="新注册用户" value={overview.newUsers} />
+      </Flex>
+
+      <Divider />
+
+      <h2>数据折线图</h2>
     </div>
   )
 }
